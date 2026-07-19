da66fb33b17ab711df03ee494043444a12adbacf
ajaysingh132 <banjarajanmat@gmail.com>
2026-07-19 06:37:02 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://dvsdqnhjucguavevcfjq.supabase.co";
 
-const SUPABASE_ANON_KEY = "यहाँ अपनी Publishable Key (sb_publishable...) पेस्ट करें";
+const SUPABASE_ANON_KEY = sb_publishable_ZAtw7wfzfUAve2C-22qepA_vUMRDxpT
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
